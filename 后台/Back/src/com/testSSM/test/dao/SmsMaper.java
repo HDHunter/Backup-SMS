b61483e88d85f6bb5b6854a522efef9ffc5effc2
zhangjianqiu <zhangjianqiu007@126.com>
2023-06-23 10:03:44 +0800

    project restructure..run by intellij.

--- 后台/Back/src/com/testSSM/test/dao/SmsMaper.java
+++ 后台/Back/src/com/testSSM/test/dao/SmsMaper.java
@@ -1,9 +1,5 @@
 package com.testSSM.test.dao;
 
-import java.util.List;
-
-import org.springframework.stereotype.Repository;
-
 import com.testSSM.test.model.Sms;
 
 
--- 后台/Back/src/com/testSSM/test/service/ContactsSer.java
+++ 后台/Back/src/com/testSSM/test/service/ContactsSer.java
@@ -1,15 +1,11 @@
 package com.testSSM.test.service;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import com.testSSM.test.dao.ContactsMaper;
-import com.testSSM.test.dao.SmsMaper;
 import com.testSSM.test.model.Contacts;
-import com.testSSM.test.model.Sms;
 
 
 @Service
